b62386855ef0bfe62ceaa8e03ed6bb79c39e7639
Ellen Collins <colliell@flip3.engr.oregonstate.edu>
2017-01-20 12:48:01 -0800

    Update script (JSON output), removed images

--- images/scrape_images.py
+++ images/scrape_images.py
@@ -4,6 +4,7 @@ import urllib
 from collections import namedtuple
 import time
 import re
+import json
 from bs4 import BeautifulSoup
 
 RETRIEVE_FROM = 'https://www.wunderground.com/wximage/imagegallery'
@@ -14,13 +15,15 @@ if __name__ == '__main__':
     print "Starting retrieval"
     if len(sys.argv) > 1:
         RETRIEVE_FROM = sys.argv[1]
-    ImageData = namedtuple('ImageData', 'img, link, date')
+    ImageData = namedtuple('ImageData', 'file, img, link, date')
+    IMG_FIELDS = ['file', 'img', 'link', 'date']
     DATA_LIST = []
     URL = urllib.urlopen(RETRIEVE_FROM).read()
     soup = BeautifulSoup(URL, "lxml")
     links = soup.findAll('a', class_='photo-link')
     print "Following image links"
     print "     Found %d links" % (len(links))
+    i = 1
     for link in links:
         full_link = "https://www.wunderground.com/" + link.get('href')
         URL = urllib.urlopen(full_link).read()
@@ -32,19 +35,17 @@ if __name__ == '__main__':
             full_image = image.get('data-interchange')
             if full_image is not None:
                 full_image = "https:" + image.nextSibling.nextSibling.img['src']
-                # print "     %s" % (full_image)
-                data = ImageData(full_image, full_link, full_date)
-                DATA_LIST.append(data)
+                print "     %s" % (full_image)
+                filename = str(int(time.time())) + "_" + str(i) + "_" + full_image.split('/')[-1]
+                if filename.endswith(IMAGE_TYPE):
+                    i = i + 1
+                    data = ImageData(filename, full_image, full_link, full_date)
+                    DATA_LIST.append(data)
     print "Writing to file and downloading images"
     DATA_FILE = open('image_data' + str(int(time.time())) + ".txt", 'w')
-    DATA_FILE.write("Date taken\tFilename\tImage source\tPage link\n")
-    i = 1
     for data in DATA_LIST:
-        filename = str(int(time.time())) + "_" + str(i) + "_" + data.img.split('/')[-1]
-        if filename.endswith(IMAGE_TYPE):
-            # print "     " + filename
-            urllib.urlretrieve(data.img, filename)
-            DATA_FILE.write(data.date + "\t" + filename + "\t" + data.img + "\t" + data.link + "\n")
-            i = i + 1
+        print "     " + data.file
+        urllib.urlretrieve(data.img, data.file)
+        DATA_FILE.write(json.dumps(dict(zip(IMG_FIELDS, data)), sort_keys=True, indent=4))
     DATA_FILE.close()
     print "Done"
