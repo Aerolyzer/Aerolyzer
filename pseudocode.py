0a70b8e2c308c04c197198f4eb02395cea1888fe
Jesse Hanson <hansojes@flip1.engr.oregonstate.edu>
2017-02-24 12:33:19 -0800

    updated parsing of file_size, error messages, and get_exif

--- pseudocode.py
+++ pseudocode.py
@@ -15,7 +15,14 @@ outlined below.
 '''
 
 '''
-Purpose:        The purpose of this file is to keep all image criteria specifications located in
+import libraries
+'''
+import re
+import exifread
+
+
+'''
+Purpose:        The purpose of this section is to keep all image criteria specifications located in
                 one place, for simplicity's sake.
 Inputs:         None
 Outputs:        None
@@ -24,11 +31,30 @@ Assumptions:    These values will be populated by a developer.
 '''
 acceptedFileTypes = ["JPEG", "JPG", "PNG"]
 acceptedMobileDevices = ["i5", "i5s", "i6", "i6s", "i7"]
-imgMaxSize = 200;
+imgMaxSizeNumber = 200;
+imgMaxSizeBytesShort = "kb"
+imgMaxSizeBytesLong = "kilobytes"
+img_sizes_long =  ['0': 'bytes',
+                    '1': 'kilobytes',
+                    '2': 'gigabytes',
+                    '3': 'megabytes',
+                    '4': 'terrabytes']
+img_sizes_short =    ['0': 'b',
+                        '1': 'kb',
+                        '2': 'gb',
+                        '3': 'mb',
+                        '4': 'tb']
 imgWidthMin = xxx;
 imgHeightMin = xxx;
 imgWidthMax = xxx;
 imgHeightMax = xxx;
+imgRestrictionErrorText =  {'accept_device': 'The image must be a mobile image from a supported device',
+                            'is_edited': 'The image cannot be edited or filtered in any way',
+                            'is_landscape': 'The image must be of a direct landscape with a sky and view',
+                            'accept_size': 'The image must be no larger than 200kb',
+                            'accept_file_type': 'The file type of the image must be .jpg or .png',
+                            'accept_resolution': 'The image must be in the resolution range 1X1-1000X1000',
+                            'is_location_services': 'Location services must be enabled for the camera'}
 
 '''
 Purpose:        The purpose of this function is to determine whether or not the device the
@@ -40,7 +66,7 @@ Assumptions:    N/A
 '''
 def accept_device(exifData, acceptedMobileDevices):
     #check to make sure the key-value pair exists in exifData to prevent errors
-    if (!('DEVICE' in exifData.keys())):
+    if ('DEVICE' not in exifData.keys()):
         return false
     
     if any(exifData['DEVICE'].lower() in device for device in acceptedMobileDevices):
@@ -57,7 +83,7 @@ Assumptions:    N/A
 '''
 def is_edited(exifData):
     #check to make sure the key-value pair exists in exifData to prevent errors
-    if (!('Create Date' in exifData.keys()) or !('Modify Date' in exifData.keys())):
+    if ('Create Date' not in exifData.keys() or 'Modify Date' not in exifData.keys()):
         return false
     
     if (exifData['Create Date'] == exifData['Modify Date']):
@@ -84,15 +110,50 @@ Outputs:        None
 Returns:        Boolean
 Assumptions:    N/A
 '''
-def accept_size(exifData, imgMaxSize):
+def accept_size(exifData, imgMaxSizeNumber, imgMaxSizeBytesShort, imgMaxSizeBytesLong, img_sizes_long, img_sizes_short):
     #check to make sure the key-value pair exists in exifData to prevent errors
-    if(!('File Size' in exifData.keys())):
+    if('File Size' not in exifData.keys()):
         return false
+  
+    #parse File Size parameter into a list of the numbers, spaces, and letters
+    file_parameters = parse_file_size(exifData['File Size'])
     
-    if (exifData['File Size'] <= imgMaxSize): #parsing may be necessary here "200 kB"
-        return true
-    else:
+    #check to make sure parsing was successful
+    if(file_parameters == [] or len(file_parameters) != 3):
         return false
+    else:
+        #Case 1: File Size has same byte-type as the maximum value
+        if(file_parameters[2].lower() == imgMaxSizeBytesShort or file_parameters[2].lower() == imgMaxSizeBytesLong):
+            if(int(file_parameters[0]) <= imgMaxSize):
+                return true
+            else:
+                return false
+        else:                
+            #Determine img byte-size key
+            for key, value in img_sizes_long.iteritems()
+                if value = file_parameters[2].lower():
+                    img_key = key
+            
+            for key, value in img_sizes_short.iteritems()
+                if value = file_parameters[2].lower():
+                    img_key = key
+            
+            #Determine accepted img size byte-size key
+            for key, value in img_sizes_short.iteritems()
+                if value = imgMaxSizeBytes:
+                    accept_img_key = key
+
+            #Case 2: File Size byte-type is smaller
+            if(img_key < accept_img_key):
+                return true
+
+            #Case 3: File Size byte-type is larger
+            elif(img_key > accept_img_key):
+                return false
+
+            #Case 4: File Size byte-type unrecognized
+            else:
+                return false
 
 '''
 Purpose:        The purpose of this function  is to determine whether or not the image is
@@ -104,7 +165,7 @@ Assumptions:    N/A
 '''
 def accept_file_type(exifData, acceptedFileTypes):
     #check to make sure the key-value pair exists in exifData to prevent errors
-    if(!('File Type' in exifData.keys())):
+    if('File Type' not in exifData.keys()):
         return false
 
     if any(exifData['File Type'].upper() in fileType for fileType in acceptedFileTypes):
@@ -120,13 +181,13 @@ Outputs:        None
 Returns:        Boolean
 Assumptions:    N/A
 '''
-def accept_resolution(exifData, imgWidthMin, imgHeightMin):
+def accept_resolution(exifData, imgWidthMin, imgHeightMin, imgWidthMax, imgHeightMax):
     #check to make sure the key-value pair exists in exifData to prevent errors
-    if(!('Exif Image Width' in exifData.keys()) or !('Exif Image Height'in exifData.keys())):
+    if('Exif Image Width' not in exifData.keys() or 'Exif Image Height'in exifData.keys()):
         return false
     
-    if (exifData['Exif Image Width'] >= imgWidthMin  && exifData['Exif Image Height'] >= imgHeightMin):
-        if (exifData['Exif Image Width'] <= imgWidthMax  && exifData['Exif Image Height'] <= imgHeightMax):
+    if (exifData['Exif Image Width'] >= imgWidthMin and exifData['Exif Image Height'] >= imgHeightMin):
+        if (exifData['Exif Image Width'] <= imgWidthMax and exifData['Exif Image Height'] <= imgHeightMax):
             return true
     else:
         return false
@@ -140,10 +201,80 @@ Returns:        Boolean
 Assumptions:    N/A
 '''
 def is_location_services(exifData):
-    if(!('GPS Latitude' in exifData.keys()) or !('GPS Longitude' in exifData.keys())):
+    if('GPS Latitude' not in exifData.keys() or 'GPS Longitude' not in exifData.keys()):
         return false
     
     if (exifData['GPS Latitude'] != '' and exifData['GPS Longitude'] != ''):
         return true
     else:
         return false
+
+'''
+Purpose:        The purpose of this function is to parse the file_size value into numbers
+                and letters.
+Inputs:         string file_size
+Outputs:        None
+Returns:        list file_parameters (digits, spaces, letters)
+Assumptions:    N/A
+'''
+def parse_file_size(file_size):
+    match = re.match(r"([0-9]+)(\s*)([a-z]+)$", file_size, re.I)
+    if match:
+        file_parameters = match.groups()
+    else:
+        file_parameters = []
+    return(file_parameters)
+
+
+'''
+Purpose:        The purpose of this function is to determine whether or not an image
+                restriction was passed, and print out the correct error statement accordingly.
+Inputs:         boolean bool, list imgRestrictionErrorText, key error
+Outputs:        None
+Returns:        Error message or blank message
+Assumptions:    N/A
+'''
+def error_message(boolean, imgRestrictionErrorText, error):
+    if(!boolean):
+        return("Restriction Error: %s." % (imgRestrictionErrorText[error]))
+    else:
+        return('')
+
+'''
+Purpose:        The purpose of this main function is to check all image restrictions and
+                produce the correct error message should one occur.
+Inputs:         None
+Outputs:        None
+Returns:        N/A
+Assumptions:    N/A
+'''
+def get_exif(path)
+    img = open(path, 'rb')
+    tags = exifread.process_file(img)
+    return tags
+
+'''
+Purpose:        The purpose of this main function is to check all image restrictions and
+                produce the correct error message should one occur.
+Inputs:         None
+Outputs:        None
+Returns:        N/A
+Assumptions:    N/A
+'''
+def main():
+    #Retrieve exif data
+    exifData = get_exif(path)
+
+    #Call each function and check for false return values
+    error_message(accept_device(exifData, acceptedMobileDevices), imgRestrictionErrorText, 'accept_device')
+    error_message(is_edited(exifData), imgRestrictionErrorText, 'is_edited')
+    error_message(is_landscape(exifData), imgRestrictionErrorText, 'is_landscape')
+    error_message(accept_size(exifData, imgMaxSize), imgRestrictionErrorText, 'accept_size')
+    error_message(accept_file_type(exifData, acceptedFileTypes), imgRestrictionErrorText, 'accept_file_type')
+    error_message(accept_resolution(exifData, imgWidthMin, imgHeightMin, imgWidthMax, imgHeighMax), imgRestrictionErrorText, 'accept_resolution')
+    error_message(is_location_services(exifData), imgRestrictionErrorText, 'is_location_services')
+
+
+
+
+main()
