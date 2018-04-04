5a0e9cdeb1e350158aa9568fca36fb8a600bb2c0
wingarlo <wingarlo@oregonstate.edu>
2018-04-04 16:03:34 -0700

    Color extraction get_rgb function (#119)
    * These are the changes from last pull request
    
    * last changes from previous pull request
    
    * start of color extraction
    
    * get_rgb function now outputs list of list of hsv values
    
    * get_rgb now chooses 1000 random points to pass to increase speed

--- aerolyzer/retrieve_image_data.py
+++ aerolyzer/retrieve_image_data.py
@@ -7,6 +7,7 @@ import os
 import cv2
 import yaml
 import exifread
+import numpy as np
 from datetime import datetime
 
 class RtrvData(object):
@@ -56,8 +57,8 @@ class RtrvData(object):
         return tags
 
     '''
-    Purpose:        The purpose of this function is to retrieve the RGB values from an
-                    image.
+    Purpose:        The purpose of this function is to retrieve the HSV values from an
+                    image's haze layer.
     Inputs:         string pathname
     Outputs:        None
     Returns:        tuple of lists of lists containing RGB values
@@ -65,7 +66,48 @@ class RtrvData(object):
     '''
     def get_rgb(self, pathname):
         img = cv2.imread(pathname,1)
-        return img
+        mask = np.zeros(img.shape[:2], np.uint8)
+        mask[0:(img.shape[0] / 2), 0:img.shape[1]] = 255
+        masked_img = cv2.bitwise_and(img, img, mask = mask)
+
+        # Create histograms with 16 bins in range 0-255
+        color = ('b', 'g', 'r')
+        b, g, r = cv2.split(img)
+        dimy, dimx = img.shape[:2]
+
+        largest = [0, 0]
+        it = dimy / 200 #iterations = total number of rows(pixels) / 200
+        for i in range(dimy / 6, (dimy / 6) * 5, it):   #only looking at the middle half of the image
+            ravg = (sum(r[i]) / float(len(r[i])))
+            gavg = (sum(g[i]) / float(len(g[i])))
+            bavg = (sum(b[i]) / float(len(b[i])))
+            avg = (ravg + gavg + bavg) / 3
+            pravg = (sum(r[i - it]) / float(len(r[i - it])))
+            pgavg = (sum(g[i - it]) / float(len(g[i - it])))
+            pbavg = (sum(b[i - it]) / float(len(b[i - it])))
+            pavg = (pravg + pgavg + pbavg) / 3
+            diff = pavg - avg
+            if diff > largest[0]:   #only getting the largest intensity drop.
+                largest = [diff,i-(it/2)]
+        sky = img[0:largest[1], 0:dimx]#cropping out landscape
+        h2 = sky[(sky.shape[0] / 4)*3:(sky.shape[0]), 0:dimx]#bottom half
+        hsv = cv2.cvtColor(h2, cv2.COLOR_BGR2HSV)
+        clrlst = []
+        dimy, dimx = h2.shape[:2]
+        for t in range(1000):
+            temp = []
+            x = int(np.random.random()*10000) % dimx
+            y = int(np.random.random()*10000) % dimy
+            for k in xrange(len(hsv[y][x])):
+                temp.append(hsv[y][x][k])
+            clrlst.append(temp)
+        '''for i in xrange(len(hsv)):
+            for j in xrange(len(hsv[i])):
+                a = []
+                for k in xrange(len(hsv[i][j])):
+                    a.append(hsv[i][j][k])
+                clrlst.append(a)'''
+        return clrlst
 
     '''
     Purpose:        The purpose of this function is to import the contents of the configuration file.
---
+++ aerolyzer/test-clrext.py
@@ -0,0 +1,7 @@
+import os
+from retrieve_image_data import RtrvData
+
+test = RtrvData("./images/img6.jpg")
+test.get_rgb("./images/img6.jpg")
+
+
